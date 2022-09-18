883248c67121bc34bcca2d273990ce683789c3e8
Mark Estrada <mark.estrada.dev>
2022-09-18 18:14:09 -0500

    add csv parsing and basic conditional logic

--- build/index.js
+++ build/index.js
@@ -2,5 +2,43 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const node_fs_1 = require("node:fs");
 // macOS, Linux, and Windows
-const newFile = (0, node_fs_1.readFileSync)('/football.csv');
-console.log(newFile);
+// MY CODE
+// const matches = readFileSync('football.csv', { encoding: 'utf8' });
+// const lines = matches.split('\n')
+// const matchesList = []
+// lines.forEach((line) => {
+//   matchesList.push(Array(line.split(',')));
+// })
+// matchesList.forEach((line) => {
+//   console.log(line)
+// })
+// console.log(matchesList)
+// HIS CODE (RAW APPROACH)
+const matches = (0, node_fs_1.readFileSync)('football.csv', { encoding: 'utf8' })
+    .split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+//REFACTOR TO CLARIFY CONDITIONAL LOGIC (VARIABLES)
+// const matchResult = {
+//   homeWin: 'H',
+//   awayWin: 'A',
+//   draw: 'D',
+// }
+//REFACTOR TO ENUM (BASICALLY ANNOTATION TO OTHER ENGINEERS?)
+var matchResult;
+(function (matchResult) {
+    matchResult["HomeWin"] = "H";
+    matchResult["awayWin"] = "A";
+    matchResult["Draw"] = "D";
+})(matchResult || (matchResult = {}));
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === matchResult.awayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Manchester United Wins: ${manUnitedWins}`);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,50 @@
 import { readFileSync } from 'node:fs';
 
 // macOS, Linux, and Windows
-const newFile = readFileSync('/football.csv');
+// MY CODE
+// const matches = readFileSync('football.csv', { encoding: 'utf8' });
 
-console.log(newFile)
+// const lines = matches.split('\n')
+// const matchesList = []
+// lines.forEach((line) => {
+//   matchesList.push(Array(line.split(',')));
+// })
+
+// matchesList.forEach((line) => {
+//   console.log(line)
+// })
+
+// console.log(matchesList)
+
+// HIS CODE (RAW APPROACH)
+
+const matches = readFileSync('football.csv', { encoding: 'utf8' })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',')
+  })
+
+//REFACTOR TO CLARIFY CONDITIONAL LOGIC (VARIABLES)
+// const matchResult = {
+//   homeWin: 'H',
+//   awayWin: 'A',
+//   draw: 'D',
+// }
+
+//REFACTOR TO ENUM (BASICALLY ANNOTATION TO OTHER ENGINEERS?)
+enum matchResult {
+  HomeWin = 'H',
+  awayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
+    manUnitedWins++
+  } else if (match[2] === 'Man United' && match[5] === matchResult.awayWin) {
+    manUnitedWins++
+  }
+}
+
+console.log(`Manchester United Wins: ${manUnitedWins}`)
